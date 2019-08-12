15fb4026a20a758b2ce37afd013353106efaaadb
Daniel Peng <30786057+DanielPeng2@users.noreply.github.com>
2019-08-11 20:38:32 -0400

    Create module to extract data from gpx files

--- backend/index.js
+++ backend/index.js
@@ -1,1 +1,10 @@
-console.log('hello')
+const fs = require('fs')
+const gpxParser = require('./gpx_parser.js')
+
+const data = fs.readFileSync("Carnival_10k.gpx")
+const demo = async() => {
+  const test = await gpxParser.parse(data.toString())
+  console.log(test)
+}
+
+demo()
