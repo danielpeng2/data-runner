19b932bbe4de93659e494548d1c4d226d3f501d7
Daniel Peng <30786057+DanielPeng2@users.noreply.github.com>
2019-08-16 23:07:23 -0400

    Add json transform to activity schema

--- backend/models/activity.js
+++ backend/models/activity.js
@@ -13,4 +13,12 @@ const activitySchema = new mongoose.Schema({
   time: Number,
 })
 
+activitySchema.set('toJSON', {
+  transform: (document, returnedObject) => {
+    returnedObject.id = returnedObject._id.toString()
+    delete returnedObject._id
+    delete returnedObject.__v
+  }
+})
+
 module.exports = mongoose.model('Activity', activitySchema)
