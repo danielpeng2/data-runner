824b7c703bb6adad0bb6fe348bbdd32595fd3da5
Daniel Peng <30786057+DanielPeng2@users.noreply.github.com>
2019-08-20 23:10:05 -0400

    More consistent error handling in login controller

--- backend/controllers/login_controller.js
+++ backend/controllers/login_controller.js
@@ -2,34 +2,37 @@ const bcrypt = require('bcrypt')
 const jwt = require('jsonwebtoken')
 const usersRepo = require('../repositories/users_repo')
 
-const login = async(req, res) => {
+const login = async(req, res, next) => {
   const body = req.body
+  let passwordCorrect
+  let user
+  try {
+    user = await usersRepo.getUserByUsername(body.username)
+  } catch(err) {
+    return next(err)
+  }
 
-  const user = await usersRepo.getUserByUsername(body.username)
-  const passwordCorrect = user === null
-    ? false
-    : await bcrypt.compare(body.password, user.passwordHash)
+  passwordCorrect = !user ? false : await bcrypt.compare(body.password, user.passwordHash)
   
-    if (!user || !passwordCorrect) {
-      return res
-        .status(401)
-        .json({ error: 'invalid username or password' })
-    }
+  if (!user || !passwordCorrect) {
+    return res
+      .status(401)
+      .json({ error: 'invalid username or password' })
+  }
 
-    const userForToken = {
-      id: user._id,
+  const userForToken = {
+    id: user._id,
+    username: user.username,
+  }
+  const token = jwt.sign(userForToken, process.env.SECRET)
+
+  res
+    .status(200)
+    .send({
+      token,
       username: user.username,
-    }
-
-    const token = jwt.sign(userForToken, process.env.SECRET)
-
-    res
-      .status(200)
-      .send({
-        token,
-        username: user.username,
-        name: user.name,
-      })
+      name: user.name,
+    })
 }
 
 module.exports = {
