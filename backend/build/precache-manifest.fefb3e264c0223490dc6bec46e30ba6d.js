843ad1f5d08c98b701a4faccf88014a00c1af917
Daniel Peng <30786057+DanielPeng2@users.noreply.github.com>
2019-09-05 19:36:46 -0400

    Add deploy script and add track build folder in git

---
+++ backend/build/precache-manifest.fefb3e264c0223490dc6bec46e30ba6d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "72e05f54568cb12b517670cd06a99df0",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e1665773ace192f684a3",
+    "url": "/static/css/2.4427844c.chunk.css"
+  },
+  {
+    "revision": "e134eff9ae9793807fcc",
+    "url": "/static/css/main.1fc03bf0.chunk.css"
+  },
+  {
+    "revision": "e1665773ace192f684a3",
+    "url": "/static/js/2.01892041.chunk.js"
+  },
+  {
+    "revision": "e134eff9ae9793807fcc",
+    "url": "/static/js/main.daf430ef.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "0fb4ced474b46255204c85e9d02c7217",
+    "url": "/static/media/landing-background.0fb4ced4.jpg"
+  }
+]);
